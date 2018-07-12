66cb1336ec3c0c6960cfecf54eca1bee8d0ad6d3
Lucy Sutton <lucy.g.sutton@gmail.com>
2018-07-12 12:39:51 -0400

    removed alerts

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -5,71 +5,67 @@ let memberId = 0
 
 function formManipulation(){
 
-  let htmlForm = document.getElementsByTagName("form")[0]
-  htmlForm.onsubmit = formSubmit
+  let htmlForm = document.getElementsByTagName('form')[0]
+  htmlForm.onsubmit = addClick // making addClick the onsubmit function allows me to use HTML's built in form validation
 
-  let age = document.getElementsByName("age")[0]
+  let age = document.getElementsByName('age')[0]
   age.required = true
 
-  let relationship = document.getElementsByName("rel")[0]
+  let relationship = document.getElementsByName('rel')[0]
   relationship.required = true
 
-  let addButton = document.getElementsByClassName("add")[0]
-  addButton.onclick = addClick
+  let submitButton = document.getElementsByTagName('button')[1]
+  submitButton.onclick = formSubmit
 
 }
 
 function addClick(){
+
   let memberObj = {
-    age: document.getElementsByName("age")[0].value,
-    relationship: document.getElementsByName("rel")[0].value,
-    smoker: document.getElementsByName("smoker")[0].checked
+    age: document.getElementsByName('age')[0].value,
+    relationship: document.getElementsByName('rel')[0].value,
+    smoker: document.getElementsByName('smoker')[0].checked
   }
+
   let valid = validator(memberObj)
   if (valid) {
     householdObj[memberId] = memberObj
     householdDisplay(memberObj, memberId)
     memberId++
-    return false;
+    return false // prevents the form submission from refreshing the page
   } else {
     return false
   }
-  
 }
 
 function validator(obj){
   let age = obj.age
-  let relationship = obj.relationship
-  if(relationship === "") {
-    alert("relationship is required")
-    return false;
-  } 
-  if (!age) {
-    alert('age is required')
-    return false;
-  }
   if (Number(age) < 0){
-    alert('age must be greater than zero');
+    let container = document.getElementsByClassName('debug')[0]
+    container.style.display = 'flex'
+    container.innerHTML = 'age must be greater than zero'
+    container.style.color = 'red'
+    container.style.borderColor = 'red'
     return false;
+  } else {
+    return true
   }
-  else return true
 }
 
 function householdDisplay(memberObj, idNum){
-  console.log(memberObj)
-  let household = document.getElementsByClassName("household")[0]
-  let memberLi = document.createElement("li")
-  memberLi.id = memberObj.id
+
+  let household = document.getElementsByClassName('household')[0]
+
+  let memberLi = document.createElement('li')
   memberLi.appendChild(document.createTextNode(`age: ${memberObj.age}, relationship: ${memberObj.relationship}, smoker: ${memberObj.smoker}`))
   household.appendChild(memberLi)
 
-  let removeButton = document.createElement("button")
+  let removeButton = document.createElement('button')
   removeButton.appendChild(document.createTextNode('remove'))
   memberLi.appendChild(removeButton)
   removeButton.onclick = removeMember
 
   function removeMember(){
-    console.log('remove clicked')
     household.removeChild(memberLi)
     delete householdObj[idNum]
   }
@@ -77,14 +73,25 @@ function householdDisplay(memberObj, idNum){
 
 function formSubmit(){
   let json = JSON.stringify(householdObj)
-  if (json === "{}") {
-    alert("Please add household members before submitting")
-    return false;
+
+  let container = document.getElementsByClassName('debug')[0]
+  container.style.display = 'flex'
+
+  if (json === '{}') {
+    container.innerHTML = 'Please add household members before submitting'
+    container.style.color = 'red'
+    container.style.borderColor = 'red'
+    return false
   }
-  let container = document.getElementsByClassName("debug")[0]
+  // trip to the server would go in else statement, on a successful response the submitted household would be displayed
   container.innerHTML = `the following household has been submitted: 
-  ${json}`
-  container.style.display = "flex"
+
+  ${json}
+  
+Is this information incorrect? Edit your response and resubmit`
+
+  container.style.color = 'black'
+  container.style.borderColor = 'black'
   return false
 }
 
